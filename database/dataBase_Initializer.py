926c00596863a71b517aac042dbc86b4cc790307
bhuvaneshkk52 <bhuvaneshkk52@gmail.com>
2025-08-17 09:22:09 +0530

    add cloud mongodb

--- database/dataBase_Initializer.py
+++ database/dataBase_Initializer.py
@@ -2,14 +2,14 @@ import logging
 from pymongo import MongoClient
 #------------------------------------------------------------------------
 
-MONGO_USERNAME = "edveon"
-MONGO_PASSWORD = "edveon123"
-MONGO_HOST = "localhost"
-MONGO_PORT = 27017
-MONGO_DB = "MeetingRoom"
+# MONGO_USERNAME = "edveon"
+# MONGO_PASSWORD = "edveon123"
+# MONGO_HOST = "localhost"
+# MONGO_PORT = 27017
+# MONGO_DB = "MeetingRoom"
 #------------------------------------------------------------------------
 
-MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb+srv://bhuvanesh:getin@meetingroom-cluster.mzsu5gs.mongodb.net/"
 client = MongoClient(MONGO_URI)
 
 database = client.MeetingRoom
